8e2b2469bd07c0493766e3f48f9834e5cf03237c
Anton Mironov <anton.mironov@jane.app>
2023-07-20 13:32:41 -0700

    Remove temp files

--- 123.js
+++
@@ -1,7 +0,0 @@
-const semver = require('semver');
-const yaml = require('js-yaml');
-const fs = require('fs');
-
-const versionFile = yaml.load(fs.readFileSync('version.yaml', 'utf8'));
-console.log(versionFile);
-console.log(versionFile[':version'][':current']);
